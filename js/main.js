88392cdc3aca5cd05f5571a32ef03f10912a0397
curcomil <sadielsalgado98@gmail.com>
2024-03-07 20:19:11 -0600

    cambios sadiel

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ import { openPost } from './aditionals.js'
 import { searchPost } from './searchfilters.js'
 import { createPost } from './createPost.js'
 
+console.log('hola')
+
 document.addEventListener('DOMContentLoaded', () => {
 })
