ba28a57e3a1ba1d6d10302a18f5962d77cb6d6aa
DEVFBA <angenrique.gutierrez@gmail.com>
2024-03-08 19:23:48 -0600

    fix: Remove user ID when logout

--- js/logout.js
+++ js/logout.js
@@ -3,6 +3,7 @@ const btnSignOut = document.getElementById('signOutButton');
 btnSignOut.addEventListener('click', () => {
 
     localStorage.removeItem('token');
+    localStorage.removeItem('userId');
     window.location.replace('/');
 
 })
